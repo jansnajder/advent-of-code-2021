6f158788e3a6a927fa1c2a8b136eb9b0a21f6e0c
j.snajder <jan.snajder.95@gmail.com>
2022-09-25 12:56:17 +0200

    Add solution for day 4.

---
+++ sw/day_04/bingo.py
@@ -0,0 +1,111 @@
+# AoC 2021 - day 4
+# Input file shows bingo boards and bingo inputs
+#
+# Part 1: Figure out which board will win first. Calculate the final score by 
+#         multiplying sum of all unmarked numbers with the input number, which
+#         caused the board to win. What will be the final score?
+# Part 2: Figure out which board will win last. Calculate the final score by 
+#         multiplying sum of all unmarked numbers with the input number, which
+#         caused the board to win. What will be the final score?
+
+import numpy as np
+
+from typing import List, Tuple, TypeVar
+
+
+class BingoBoard:
+    '''
+    Class representing board of bingo numbers
+
+    :param raw board: 2D np array representing numbers in bingo board
+    '''
+
+    def __init__(self, raw_board: np.ndarray):
+        self.board = raw_board
+        self.won: bool = False
+
+    def mark_number(self, input: int) -> None:
+        '''
+        Check bingo board and mark the position of input if present.
+
+        :param input: bingo input
+        '''
+        if self.won is True:
+            return
+
+        self.board = np.where(self.board != input, self.board, -1)
+
+    def has_won(self) -> bool:
+        '''
+        Check if the board has won.
+
+        :return: bool indicating if the board already won
+        '''
+        if (-5 in np.sum(self.board, axis=0)) or (-5 in np.sum(self.board, axis=-1)):
+            self.won = True
+
+        return self.won
+        
+def load_data(inputs_path: str, bingo_path: str) -> Tuple[List[int], np.ndarray]:
+    '''
+    Load bingo boards and bing inputs
+
+    :param inputs_path: path to bing inputs
+    :param bingo_path: path to bingo boards
+    :return inputs: parsed bingo inputs
+    :return bingo: parsed bingo boards
+    '''
+    with open(inputs_path, 'r', encoding='utf-8') as f:
+        inputs = f.read().split(',')
+        inputs = list(map(int, inputs))
+    
+    with open(bingo_path, 'r', encoding='utf-8') as f:
+        bingo = f.read().split('\n\n')
+
+    for board_idx, bingo_board in enumerate(bingo):
+        bingo[board_idx] = np.fromstring(bingo[board_idx], dtype=int, sep=' ').reshape((5, 5))
+
+    return inputs, bingo
+
+def get_result(bingo_board: BingoBoard, input: int) -> int:
+    '''
+    Get result for given bingo board and its winning input.
+    
+    :param bingo_board: winning bingo board
+    :param input: winning bing input
+    :return: result of the task
+    '''
+    sum = 0
+
+    for row in bingo_board.board:
+        for number in row:
+            if number > 0:
+                sum += number
+
+    return sum*input
+
+
+if __name__ == '__main__':
+    inputs_path = 'day_04/inputs.txt'
+    bingo_path = 'day_04/bingo_boards.txt'
+    inputs, bingo_raw = load_data(inputs_path, bingo_path)
+
+    bingo_boards = []
+
+    for board in bingo_raw:
+        bingo_boards.append(BingoBoard(board))
+
+    winners = []
+
+    for input in inputs:
+        for board in bingo_boards:
+            board.mark_number(input)
+
+            if not board.won:
+                win = board.has_won()
+
+                if win:
+                    winners.append((board, input))
+        
+    print(get_result(winners[0][0], winners[0][1]))
+    print(get_result(winners[-1][0], winners[-1][1]))
