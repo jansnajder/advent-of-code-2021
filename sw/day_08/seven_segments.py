6629c47ab08ae823ac8409243b7ed33097dd1104
j.snajder <jan.snajder.95@gmail.com>
2022-11-14 18:15:20 +0100

    Polish solution for day 8.

--- sw/day_08/seven_segments.py
+++ sw/day_08/seven_segments.py
@@ -1,23 +1,45 @@
+# AoC 2021 - day 3
+# Input file shows seven segments number - each segment is one letter.
+#
+# Part 1:  - decode segment with known length and count how many of
+#            them are in the input after |
+# Part 2:  - decode all segments and sum all numbers after |
+
 from typing import List, Tuple
 
 
-def load_data(path: str) -> Tuple[List[str], List[str]]:
+def load_data(path: str) -> Tuple[List[List[str]], List[List[str]]]:
+    '''
+    Load seven segments data
+
+    :param path: path to txt file
+    :return first_part: part before |
+    :return second_part: part after |
+    '''
     with open(path, 'r', encoding='utf-8') as f:
         lines_raw = f.read().split('\n')
 
     first_part = []
     second_part = []
+
     for line in lines_raw:
-        first_part.append(line.split(' | ')[0].split(' '))
-        second_part.append(line.split(' | ')[1].split(' '))
+        if ' | ' in line:
+            first_part.append(line.split(' | ')[0].split(' '))
+            second_part.append(line.split(' | ')[1].split(' '))
 
     return first_part, second_part
 
 
-def count_known_output(segments):
-    lengths = [2, 3, 4, 7]  # length of known segments
+def count_known_output(segments: List[List[str]]) -> int:
+    '''
+    Count number of known numbers in second part of input.
 
+    :param segments: segments definition
+    :return count: number of known numbers, i.e., 1, 4, 7, 8
+    '''
+    lengths = [2, 3, 4, 7]  # length of known segments
     count = 0
+
     for output in segments:
         for segment in output:
             if len(segment) in lengths:
@@ -26,40 +48,17 @@ def count_known_output(segments):
     return count
 
 
-# rules:
-# vím že dva pravé segmenty jsou jednička
-# segment pro sedmičku minus segment pro jedničku mi dá horní
-# segment pro čtyřku minus segment pro jedničku mi da prostředek a levy horni
-# segment s délkou šest, který obsahuje pět segmentů, které už jsou detekované je devítka -> dostanu spodní
-# segment s délkou šest, ten druhý, je šestka - dostanu levý spodní a pravý horní
-# jsem schopný zjistit co je dvojka a pětka a trojka
-
-#  000
-# 1   2
-# 1   2
-#  333
-# 4   4
-# 4   5
-#  666
-
-
 class SevenSegments():
+    '''
+    Class representing decoded seven segments
 
-    def __init__(self, first_part, second_part):
+    :params first_part: segments before |
+    :params second_part: segments after |
+    '''
 
-        self.zero = []
-        self.one = []
-        self.two = []
-        self.three = []
-        self.four = []
-        self.five = []
-        self.six = []
-        self.seven = []
-        self.eight = []
-        self.nine = []
-
+    def __init__(self, first_part: List[str], second_part: List[str]):
         # known numbers
-        self.get_known_number(first_part)
+        first_part = self.get_known_number(first_part)
 
         # unknown numbers
         self.get_unknown_number(first_part)
@@ -71,86 +70,86 @@ class SevenSegments():
         #
         self.get_numbers_from_output(second_part)
 
-    def get_known_number(self, segments):
-        for segment in segments:
-            if len(segment) == 2:
-                for letter in segment:
-                    self.one.append(letter)
-                self.one = sorted(self.one)
-            if len(segment) == 3:
-                for letter in segment:
-                    self.seven.append(letter)
-                self.seven = sorted(self.seven)
-            if len(segment) == 4:
-                for letter in segment:
-                    self.four.append(letter)
-                self.four = sorted(self.four)
-            if len(segment) == 7:
-                for letter in segment:
-                    self.eight.append(letter)
-                self.eight = sorted(self.eight)
+    def get_known_number(self, segments: List[str]) -> List[str]:
+        '''
+        Decode numbers with known length, i.e, 1, 4, 7, 8
 
-    def get_unknown_number(self, segments):
+        :param segments: encoded segments
+        :return segments_left: rest of encoded segments
+        '''
+        known_nums = {}
+        known_lens = (0, 2, 0, 0, 4, 0, 0, 3, 7, 0)
+        segments_left = []
+
+        for segment in segments:
+            if len(segment) in known_lens:
+                known_nums[len(segment)] = sorted([letter for letter in segment])
+            else:
+                segments_left.append(segment)
+
+        self.one = known_nums[2]
+        self.seven = known_nums[3]
+        self.four = known_nums[4]
+        self.eight = known_nums[7]
+
+        return segments_left
+
+    def get_unknown_number(self, segments: List[str]) -> None:
+        '''
+        Decode numbers, which are not unique in length.
+
+        Step by step:
+            First part  - if all segments are in 4 and 1 -> it has to be 9
+                        - if all segments are in only 1 -> it is either 3 or 0, depends on length
+
+            Second part - from remaining only 6 has six segments
+                        - 5 and 2 left -> 5 is subset of nine
+
+        :param segments: unknown segments
+        '''
+        segment_left = []
+
+        for segment in segments:
+            if all(part in segment for part in self.four) and all(part in segment for part in self.one):
+                self.nine = sorted([letter for letter in segment])
+            elif all(part in segment for part in self.one):
+                if len(segment) == 5:
+                    self.three = sorted([letter for letter in segment])
+                elif len(segment) == 6:
+                    self.zero = sorted([letter for letter in segment])
+            else:
+                segment_left.append(segment)
+
+        segments = segment_left
+
         for segment in segments:
             if len(segment) == 6:
-                if all(part in segment for part in self.four) and all(part in segment for part in self.one):
-                    for letter in segment:
-                        self.nine.append(letter)
-                    self.nine = sorted(self.nine)
-            if len(segment) == 5:
-                if all(part in segment for part in self.one):
-                    for letter in segment:
-                        self.three.append(letter)
-                    self.three = sorted(self.three)
+                self.six = sorted([letter for letter in segment])
+            else:
+                if all(part in self.nine for part in segment):
+                    self.five = sorted([letter for letter in segment])
+                else:
+                    self.two = sorted([letter for letter in segment])
 
-        for segment in segments:
-            if len(segment) == 6:
-                if all(part in segment for part in self.seven) and not all(part in segment for part in self.nine):
-                    for letter in segment:
-                        self.zero.append(letter)
-                    self.zero = sorted(self.zero)
+    def get_numbers_from_output(self, segments):
+        '''
+        Calculate one number from second part of segments - e.g. 8, 9, 0 -> 890
 
-        for segment in segments:
-            if len(segment) == 6:
-                if not all(part in segment for part in self.zero) and not all(part in segment for part in self.nine):
-                    for letter in segment:
-                        self.six.append(letter)
-                    self.six = sorted(self.six)
-
-        for segment in segments:
-            if len(segment) == 5:
-                if all(part in self.six for part in segment):
-                    for letter in segment:
-                        self.five.append(letter)
-                    self.five = sorted(self.five)
-
-        for segment in segments:
-            if len(segment) == 5:
-                if not all(part in segment for part in self.five) and not all(part in segment for part in self.three):
-                    for letter in segment:
-                        self.two.append(letter)
-                    self.two = sorted(self.two)
-
-    def get_numbers_from_output(self, output):
-        segment_list = []
-        for idx, segment in enumerate(output):
-            segment_list.append([])
-            for letter in segment:
-                segment_list[idx].append(letter)
-            segment_list[idx] = sorted(segment_list[idx])
-
+        :param segments: encoded segments
+        '''
         self.number = 0
-        for idx, segment in enumerate(segment_list):
+
+        for segment in segments:
+            segment_chars = sorted([letter for letter in segment])
             self.number = self.number * 10
-            self.number += self.numbers.index(segment)
+            self.number += self.numbers.index(segment_chars)
 
 
 if __name__ == '__main__':
     path = 'day_08/inputs.txt'
     first_part, second_part = load_data(path)
 
-    count = count_known_output(second_part)
-    print(count)
+    print(f'Result of part one: {count_known_output(second_part)}.')
 
     segment = []
 
@@ -162,4 +161,4 @@ if __name__ == '__main__':
     for output in segment:
         result += output.number
 
-    print(result)
+    print(f'Result of part two: {result}')
