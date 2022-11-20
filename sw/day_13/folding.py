49333423e8cdaee3d82e6d9ab013899d164cb82b
j.snajder <jan.snajder.95@gmail.com>
2022-11-20 14:06:17 +0100

    Add solution for day 13.

--- sw/day_13/folding.py
+++ sw/day_13/folding.py
@@ -1,77 +1,93 @@
-def load_data(path):
-    with open(path, 'r') as f:
-        data_raw = f.read().split('\n')
+# AoC 2021 - day 15
+# Input files show coordinates of dots and folding instruction
+#
+# Part 1:  - Fold the paper once, how many dots are left?
+# Part 2:  - Fold the paper according to all instructions and find out the secret code.
 
-    data = []
-    for line in data_raw:
-        data.append((int(line.split(',')[0]), int(line.split(',')[1])))
-
-    return data
+from typing import List, Tuple
 
 
-def create_map(data):
-    x_max = max([i[0] for i in data])
-    y_max = max([i[1] for i in data])
-    dot_map = [[0 for i in range(x_max + 1)] for j in range(y_max + 1)]
+def load_data(path_coords: str, path_instr: str) -> Tuple[List[Tuple[int, int]], List[Tuple[str, int]]]:
+    '''
+    Load data from input files, data consists of coordinates and instructions.
 
-    for row in data:
-        dot_map[row[1]][row[0]] = 1
+    :param path_coords: path to coordinates file
+    :param path_instr: path to instructions file
+    :return coords: list of coordinates
+    :return instr: list of instructions
+    '''
+    with open(path_coords, 'r') as f:
+        coords_raw = f.read().split('\n')
 
-    return dot_map
+    coords = [(int(line.split(',')[0]), int(line.split(',')[1])) for line in coords_raw if line]
+
+    with open(path_instr, 'r') as f:
+        instr_raw = f.read().split('\n')
+
+    instr = [(line.split('=')[0][-1], int(line.split('=')[1])) for line in instr_raw if line]
+
+    return coords, instr
 
 
-def fold_map(data, horizontal, line):
-    if horizontal:
+def fold_paper(coords: List[Tuple[int, int]], axis: str, line: int) -> List[Tuple[int, int]]:
+    '''
+    Fold paper according to instructions. This results in reduction of coordinates as some mirrors to other.
+
+    :param coords: list of coordinates
+    :param axis: identifier of axis of fold
+    :param line: line to fold by
+    :return coords: list of coordinates after folding
+    '''
+
+    if axis == 'x':
+        idx = 0
+    elif axis == 'y':
         idx = 1
     else:
-        idx = 0
+        raise ValueError(f'Invalid axis identifier "{axis}".')
 
-    for c_idx, coordinate in enumerate(data):
+    for c_idx, coordinate in enumerate(coords):
         if coordinate[idx] == line:
-            del data[c_idx]
-        if coordinate[idx] > line:
+            del coords[c_idx]
+        elif coordinate[idx] > line:
             if idx == 1:
-                data[c_idx] = (coordinate[0], line * 2 - coordinate[idx])
-            if idx == 0:
-                data[c_idx] = (line * 2 - coordinate[idx], coordinate[1])
+                coords[c_idx] = (coordinate[0], line * 2 - coordinate[idx])
+            elif idx == 0:
+                coords[c_idx] = (line * 2 - coordinate[idx], coordinate[1])
 
-    return data
+    return list(set(coords))
 
 
-def print_map(data):
+def print_coords(coords: List[Tuple[int, int]]) -> None:
+    '''
+    Print coordinates to decode the result.
+
+    :param coords: coordinates of dots
+    '''
+    print('Result of part two:')
     print('--------------------------------------------------------')
-    x_max = max([i[0] for i in data])
-    y_max = max([i[1] for i in data])
+    x_max = max([i[0] for i in coords])
+    y_max = max([i[1] for i in coords])
     dot_map = [['.' for i in range(x_max + 1)] for j in range(y_max + 1)]
-    for coordinate in data:
+
+    for coordinate in coords:
         dot_map[coordinate[1]][coordinate[0]] = '#'
 
     for row in dot_map:
         print(row)
+
     print('--------------------------------------------------------')
 
 
 if __name__ == '__main__':
-    test_path = 'test_inputs.txt'
-    test_data = load_data(test_path)
+    path_coords = 'day_13/coordinates.txt'
+    path_instr = 'day_13/instructions.txt'
+    coords, instructions = load_data(path_coords, path_instr)
 
-    test_data = fold_map(test_data, True, 7)
-    test_data = fold_map(test_data, False, 5)
-    print_map(test_data)
+    for idx, instr in enumerate(instructions):
+        if idx == 1:
+            print(f'Result of part one: {len(coords)}')
 
-    path = 'inputs.txt'
-    data = load_data(path)
+        coords = fold_paper(coords, instr[0], instr[1])
 
-    data = fold_map(data, False, 655)
-    data = fold_map(data, True, 447)
-    data = fold_map(data, False, 327)
-    data = fold_map(data, True, 223)
-    data = fold_map(data, False, 163)
-    data = fold_map(data, True, 111)
-    data = fold_map(data, False, 81)
-    data = fold_map(data, True, 55)
-    data = fold_map(data, False, 40)
-    data = fold_map(data, True, 27)
-    data = fold_map(data, True, 13)
-    data = fold_map(data, True, 6)
-    print_map(data)
+    print_coords(coords)
